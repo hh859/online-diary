1431c38aeb1802932f20a8207782e750e381d0e2
hh859 H <hanna.hirenka@gmail.com>
2024-03-03 12:48:17 +0000

    updated app.py

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ def add_task():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
